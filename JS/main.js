6c25bc04c4b8bc4c74ae6199a609553e8772d5f4
azllzlaa <azilazulwi3005@gmail.com>
2025-03-19 11:40:58 +0700

    [ini portofolio]

---
+++ JS/main.js
@@ -0,0 +1,31 @@
+// console.log("halo guysssss")
+// document.write("aku seneng bgt hari ini")
+
+// let nama;
+// nama = "Armando Mendoza";
+// console.log(nama);
+// nama = "ajil";
+// console.log(nama);
+
+// let x =2;
+// let y = 3;
+// console.log(x+y);
+// console.log(x-y+x-x);
+
+let namaaja = "alimah";
+console.log(namaaja)
+
+namaaja = 3030
+console.log(namaaja)
+
+let isFemale = true;
+console.log(typeof(isFemale));
+console.log(typeof(namaaja));
+
+
+
+let kotak = null;
+// var nama3
+// const ;
+
+
